d51b72839affa3cd078ba0527af45cf42529699b
Ke2777 <74142541+Ke2777@users.noreply.github.com>
2022-09-16 15:28:20 +0300

    Add files via upload

---
+++ Часы/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Часы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.On = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Fin = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.prep = new System.Windows.Forms.PictureBox();
+            this.prep2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prep2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(34, 173);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(34, 246);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(34, 316);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(34, 386);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(34, 455);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(34, 530);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // On
+            // 
+            this.On.Location = new System.Drawing.Point(460, 28);
+            this.On.Name = "On";
+            this.On.Size = new System.Drawing.Size(152, 63);
+            this.On.TabIndex = 7;
+            this.On.Text = "Начать";
+            this.On.UseVisualStyleBackColor = true;
+            this.On.Click += new System.EventHandler(this.On_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.Timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.Timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Tick += new System.EventHandler(this.Timer6_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 700);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Fin
+            // 
+            this.Fin.Image = ((System.Drawing.Image)(resources.GetObject("Fin.Image")));
+            this.Fin.Location = new System.Drawing.Point(884, 173);
+            this.Fin.Name = "Fin";
+            this.Fin.Size = new System.Drawing.Size(58, 407);
+            this.Fin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Fin.TabIndex = 8;
+            this.Fin.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(379, 69);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Тараканьи бега";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 63);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Новая игра";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // prep
+            // 
+            this.prep.Image = ((System.Drawing.Image)(resources.GetObject("prep.Image")));
+            this.prep.Location = new System.Drawing.Point(322, 173);
+            this.prep.Name = "prep";
+            this.prep.Size = new System.Drawing.Size(58, 407);
+            this.prep.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.prep.TabIndex = 11;
+            this.prep.TabStop = false;
+            // 
+            // prep2
+            // 
+            this.prep2.Image = ((System.Drawing.Image)(resources.GetObject("prep2.Image")));
+            this.prep2.Location = new System.Drawing.Point(615, 173);
+            this.prep2.Name = "prep2";
+            this.prep2.Size = new System.Drawing.Size(58, 407);
+            this.prep2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.prep2.TabIndex = 12;
+            this.prep2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 609);
+            this.Controls.Add(this.prep2);
+            this.Controls.Add(this.prep);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Fin);
+            this.Controls.Add(this.On);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prep2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button On;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Fin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox prep;
+        private System.Windows.Forms.PictureBox prep2;
+    }
+}
+
---
+++ Часы/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Часы
+{
+    public partial class Form1 : Form
+    {
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Parent = pictureBox1;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox3.Parent = pictureBox1;
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox4.Parent = pictureBox1;
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox5.Parent = pictureBox1;
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox6.Parent = pictureBox1;
+            pictureBox6.BackColor = Color.Transparent;
+            pictureBox7.Parent = pictureBox1;
+            pictureBox7.BackColor = Color.Transparent;
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+            prep.Visible = false;
+            prep2.Visible = false;
+            rnd = new Random();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox2.Left += 1;
+            int value1 = rnd.Next(10, 25);
+            this.timer1.Interval = value1;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer1.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer1.Stop();
+                }
+            }
+            if (pictureBox2.Bounds.IntersectsWith(Fin.Bounds))
+            {
+
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 1-ый", "Победа");
+            }
+        }
+
+        private void On_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+            this.timer2.Enabled = true;
+            this.timer3.Enabled = true;
+            this.timer4.Enabled = true;
+            this.timer5.Enabled = true;
+            this.timer6.Enabled = true;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Left += 1;
+            int value2 = rnd.Next(10, 25);
+            this.timer2.Interval = value2;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer2.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer2.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(Fin.Bounds))
+            {
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 2-ой","Победа");
+            }
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            pictureBox4.Left += 1;
+            int value3 = rnd.Next(10, 25);
+            this.timer3.Interval = value3;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer3.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer3.Stop();
+                }
+            }
+            if (pictureBox4.Bounds.IntersectsWith(Fin.Bounds))
+            {
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 3-ий", "Победа");
+            }
+
+        }
+
+        private void Timer4_Tick(object sender, EventArgs e)
+        {
+            pictureBox5.Left += 1;
+            int value4 = rnd.Next(10, 25);
+            this.timer4.Interval = value4;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer4.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer4.Stop();
+                }
+            }
+            if (pictureBox5.Bounds.IntersectsWith(Fin.Bounds))
+            {
+
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 4-ый", "Победа");
+            }
+        }
+
+        private void Timer5_Tick(object sender, EventArgs e)
+        {
+            pictureBox6.Left += 1;
+            int value5 = rnd.Next(10, 25);
+            this.timer5.Interval = value5;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer5.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer5.Stop();
+                }
+            }
+            if (pictureBox6.Bounds.IntersectsWith(Fin.Bounds))
+            {
+
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 5-ый", "Победа");
+            }
+        }
+
+        private void Timer6_Tick(object sender, EventArgs e)
+        {
+            pictureBox7.Left += 1;
+            int value6 = rnd.Next(10, 25);
+            this.timer6.Interval = value6;
+            if (pictureBox3.Bounds.IntersectsWith(prep.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer6.Stop();
+                }
+            }
+            if (pictureBox3.Bounds.IntersectsWith(prep2.Bounds))
+            {
+                if (rnd.Next(1, 500) == 1)
+                {
+                    timer6.Stop();
+                }
+            }
+            if (pictureBox7.Bounds.IntersectsWith(Fin.Bounds))
+            {
+
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+                this.timer4.Enabled = false;
+                this.timer5.Enabled = false;
+                this.timer6.Enabled = false;
+                MessageBox.Show("Победил 6-ой", "Победа");
+            }
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
